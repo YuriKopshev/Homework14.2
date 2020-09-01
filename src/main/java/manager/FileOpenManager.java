82a6fe7f4a12c171492d61298baf745aa8e74c4e
Yuri Kopshev <yura.kopshev@gmail.com>
2020-09-01 13:04:21 +0200

    Added all files

---
+++ src/main/java/manager/FileOpenManager.java
@@ -0,0 +1,40 @@
+package manager;
+
+
+import java.util.*;
+
+public class FileOpenManager {
+
+    private Map<String, String> app = new HashMap<>();
+
+    public FileOpenManager(Map<String, String> app) {
+        this.app = app;
+    }
+
+    public void add(String extension, String appName) {
+        app.put(extension.toLowerCase(), appName);
+    }
+
+    public String getAppName(String extension) {
+        return app.get(extension.toLowerCase());
+    }
+
+    public void deleteLink(String extension) {
+        app.remove(extension);
+    }
+
+    public List<String> getAllExtension(Map<String, String> map) {
+        List<String> list = new ArrayList<>(map.keySet());
+        return list;
+    }
+
+    public Set<String> getAllAppName(Map<String, String> map) {
+        List<String> list = new ArrayList<>(map.values());
+        Set<String> set = new HashSet<>();
+        set.addAll(list);
+        return set;
+
+    }
+
+
+}
---
+++ src/test/java/manager/FileOpenManagerTest.java
@@ -0,0 +1,83 @@
+package manager;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.hamcrest.collection.IsMapContaining;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.not;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+
+import java.util.*;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class FileOpenManagerTest {
+    Map<String, String> app = new HashMap<>();
+    FileOpenManager manager = new FileOpenManager(app);
+    Map<String, String> expected = new HashMap<>();
+
+    @BeforeEach
+    void setUp() {
+        expected.put("jpg", "Paint");
+        expected.put("txt", "Notepad");
+        expected.put("docx", "Notepad");
+        expected.put("rtf", "Word");
+        expected.put("pdf", "Acrobat Reader");
+    }
+
+    @Test
+    void add() {
+        manager.add("Jpg", "Paint");  //Проверяю параллельно на разном регистре
+        manager.add("txt", "Notepad");
+        manager.add("Docx", "Notepad"); //Проверяю параллельно на разном регистре
+        manager.add("rtf", "Word");
+        manager.add("pdf", "Acrobat Reader");
+        assertThat(app, is(expected));
+    }
+
+    @Test
+    void getAppName() {
+        manager.add("rtf", "Word");
+        manager.add("pdf", "Acrobat Reader");
+        String expected = "Word";
+        String actual = manager.getAppName("Rtf"); //Проверяю параллельно на разном регистре
+        assertEquals(expected, actual);
+
+    }
+
+    @Test
+    void deleteLink() {
+        manager.add("Jpg", "Paint");
+        manager.add("txt", "Notepad");
+        manager.add("Docx", "Notepad");
+        manager.add("rtf", "Word");
+        manager.add("pdf", "Acrobat Reader");
+        manager.deleteLink("jpg");
+        assertThat(app, not(IsMapContaining.hasValue("Paint")));
+
+    }
+
+    @Test
+    void getAllExtension() {
+        manager.add("Jpg", "Paint");
+        manager.add("txt", "Notepad");
+        manager.add("Docx", "Notepad");
+        Collection<String> expected = List.of("jpg", "txt", "docx");
+        Collection<String> actual = manager.getAllExtension(app);
+        assertEquals(expected, actual);
+
+
+    }
+
+    @Test
+    void getAllAppName() {
+        manager.add("Jpg", "Paint");
+        manager.add("txt", "Notepad");
+        manager.add("Docx", "Notepad");
+        Set<String> expected = new HashSet<>(Arrays.asList("Paint", "Notepad"));
+        Set<String> actual = manager.getAllAppName(app);
+        assertEquals(expected, actual);
+    }
+}
